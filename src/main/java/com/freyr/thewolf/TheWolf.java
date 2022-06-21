1d3a0ada3a4d407360690d5c9597709ad43a1d5e
bladedurman <vedantvgrover@gmail.com>
2022-06-21 12:54:58 -0700

    Created a Welcome Message

--- src/main/java/com/freyr/thewolf/TheWolf.java
+++ src/main/java/com/freyr/thewolf/TheWolf.java
@@ -7,33 +7,62 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
+import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
 
+/**
+ * Main class where we initialize the bot
+ *
+ * @author Freyr
+ */
 public class TheWolf {
 
-    private final Dotenv config;
-    private final ShardManager shardManager;
+    private final @NotNull Dotenv config; // Getting all of my sensitive info from environment file
+    private final @NotNull ShardManager shardManager; // Allows bot to run on multiple servers. Bot "builder"
 
     public TheWolf() throws LoginException {
-        config = Dotenv.configure().ignoreIfMissing().load();
+        config = Dotenv.configure().ignoreIfMissing().load(); // Initializing and loading the .env file if it exists in the classpath.
 
-        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(config.get("TOKEN", System.getenv("TOKEN")));
-        builder.setStatus(OnlineStatus.ONLINE);
-        builder.setActivity(Activity.watching("Freyr fail..."));
-        builder.enableIntents(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES);
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(config.get("TOKEN", System.getenv("TOKEN"))); // Creating a basic instance of the bot and logging in with token
+        builder.setStatus(OnlineStatus.ONLINE); // Setting the bot status to ONLINE (Green Dot)
+        builder.setActivity(Activity.watching("Freyr fail...")); // Setting the bot activity to "Freyr fail...." (I will change this)
+        builder.enableIntents(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES); // Enabling Gateway Intents for the bot to have more access to user information (ROLES, MESSAGES)
 
-        shardManager = builder.build();
+        shardManager = builder.build(); // Creating the bot
+
+        // Registering Listeners
         shardManager.addEventListener(
                 new GuildListener()
         );
     }
 
     public static void main(String[] args) {
-	    try {
-            new TheWolf();
+        try {
+            new TheWolf(); // Starting the bot
         } catch (LoginException e) {
-            System.out.println("ERROR: Provided bot token is invalid");
+            System.out.println("ERROR: Provided bot token is invalid"); // Exception handling if the bot token is invalid
         }
+    }
+
+    /**
+     * Gets the shard manager.
+     * The shard manager builds the bot and helps set its properties.
+     *
+     * @return Shard Manager
+     */
+    public @NotNull ShardManager getShardManager() {
+        return shardManager;
+    }
+
+    /**
+     * Gets the config variables
+     * The config variables are located in the .env file.
+     * These variables are secret variables and should not be shared
+     *
+     * @return Config
+     */
+    public @NotNull Dotenv getConfig() {
+        return config;
     }
 }
--- src/main/java/com/freyr/thewolf/listeners/GuildListener.java
+++ src/main/java/com/freyr/thewolf/listeners/GuildListener.java
@@ -3,43 +3,51 @@ package com.freyr.thewolf.listeners;
 import com.freyr.thewolf.util.EmbedColor;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * This is an event listener. All events that are generic to the guild happen here.
+ *
+ * @author Freyr
+ */
 public class GuildListener extends ListenerAdapter {
 
     @Override
     public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
-        //Hi hello, 多多 was here and is going to take your beans
-        //我喜欢猫咪
-        //¿tú supiste que no hablo español?
-        //Vedant 给我这个地方所以我现在在写东西
-        //Pienso que Vedant esté sospechoso, tal vez él es el impostor
+        MessageChannel channel = event.getGuild().getDefaultChannel(); // Getting the default welcome channel in a server
 
+        EmbedBuilder embed = new EmbedBuilder(); // Allows us to create and set the properties of an embed
+        embed.setColor(EmbedColor.DEFAULT_COLOR); // Sets the color of the embed to the default embed located in EmbedColor.
+        embed.setDescription(event.getMember() + " has joined " + event.getGuild().getName() + "! Remember to check " + event.getGuild().getRulesChannel() + " for full access to the server!"); // Setting the description, or the welcome message of the server
+
+        assert channel != null; // Making sure that the channel is not null
+        channel.sendMessageEmbeds(embed.build()).queue(); // Sending the welcome message
     }
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        Message msg = event.getMessage();
+        Message msg = event.getMessage(); // Getting the message from the user
 
-        final String PREFIX = "w.";
-        if (msg.getContentRaw().equals(PREFIX + "ping")) {
-            long time = System.currentTimeMillis();
-            EmbedBuilder embed = new EmbedBuilder();
-            embed.setColor(EmbedColor.DEFAULT_COLOR);
-            embed.setDescription(":signal_strength: - Calculating...");
-            event.getChannel().sendMessageEmbeds(embed.build()).queue(m -> {
-                long latency = System.currentTimeMillis() - time;
-                EmbedBuilder latencyEmbed = new EmbedBuilder();
-                latencyEmbed.setTitle(":ping_pong: - Pong!");
-                latencyEmbed.setColor(EmbedColor.DEFAULT_COLOR);
-                latencyEmbed.addField("Bot Latency", latency + "ms", false);
-                latencyEmbed.addField("Websocket", event.getJDA().getGatewayPing() + "ms", false);
-                latencyEmbed.setFooter(event.getAuthor().getName());
+        final String PREFIX = "w."; // The current prefix of the bot. Will change it to slash commands later
+        if (msg.getContentRaw().equals(PREFIX + "ping")) { // Checking to see if the command is w.ping
+            long time = System.currentTimeMillis(); // Getting the current time
+            EmbedBuilder embed = new EmbedBuilder(); // Allows us to create and set the properties of an embed
+            embed.setColor(EmbedColor.DEFAULT_COLOR); // Sets the color of the embed to the default embed color located in EmbedColor
+            embed.setDescription(":signal_strength: - Calculating..."); // Sending a message to calculate ping
+            event.getChannel().sendMessageEmbeds(embed.build()).queue(m -> { // Editing the message
+                long latency = System.currentTimeMillis() - time; // Checking the difference in time between start and message send
+                EmbedBuilder latencyEmbed = new EmbedBuilder(); // Allows us to create and set the properties of an embed
+                latencyEmbed.setTitle(":ping_pong: - Pong!"); // Setting the title to "🏓 - Pong!"
+                latencyEmbed.setColor(EmbedColor.DEFAULT_COLOR); // Sets the color of the embed to the default embed color located in EmbedColor
+                latencyEmbed.addField("Bot Latency", latency + "ms", false); // Adds the field called "Bot latency" and sets the field description to the latency
+                latencyEmbed.addField("Websocket", event.getJDA().getGatewayPing() + "ms", false); // Adds the field called "Websocket" and sets the description to the websocket latency
+                latencyEmbed.setFooter("Requested by " + event.getAuthor().getName()); // Sets the footer to the author
 
-                m.editMessageEmbeds(latencyEmbed.build()).queue();
+                m.editMessageEmbeds(latencyEmbed.build()).queue(); // Edits the message
             });
         }
     }
